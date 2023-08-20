2219c9c3018c8f60f7ef12ba31a0cc832cd6c94c
poojiroy <100796381+poojiroy@users.noreply.github.com>
2023-08-20 21:19:11 +0530

    Update ClientApp.java

--- com.AuthServer/src/main/java/com/OAuth2App/ClientApp.java
+++ com.AuthServer/src/main/java/com/OAuth2App/ClientApp.java
@@ -6,16 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientApp {
+
+	// Home Pager --poojitha
 	
 	@GetMapping("/")
 	public String showHome() {
 		return "home";
 	}
+	
+	//Managers Page Tables --poojitha
 	@GetMapping("/leaders")
 	public String leaders() {
 		return "leaders";
 	}
 	
+	//Admins Page --poojitha
 	@GetMapping("/systems")
 	public String systems() {
 		return "systems";
